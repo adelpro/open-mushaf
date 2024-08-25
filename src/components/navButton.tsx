f564b723cc7e73b92bc4d663fb3704d2ea8338d6
adelpro <adelpro@gmail.com>
2024-08-25 14:27:41 +0100

    -nav button ui fixed

--- src/components/navButton.tsx
+++ src/components/navButton.tsx
@@ -6,44 +6,57 @@ type Props = {
   toggleMenu: () => void;
 };
 const genericHamburgerLine =
-  "block md:hidden h-1 w-1 max-w-1 my-1 rounded-full bg-white transition ease transform duration-300 opacity-50 group-hover:opacity-100 w-full dark:bg-gray-100";
+  "h-1 my-1 rounded-full bg-black transition ease transform duration-300 opacity-50 group-hover:opacity-100 w-full dark:bg-gray-100";
 
 export default function NavButton({ isOpen, toggleMenu }: Props) {
   return (
     <button
       aria-label={isOpen ? "Close navigation menu" : "Open navigation menu"}
-      aria-controls="nav"
+      aria-controls="menuNav"
       aria-expanded={isOpen}
       title={isOpen ? "Close navigation menu" : "Open navigation menu"}
       role="button"
       aria-haspopup="true"
+      type="button"
+      id="openIcon"
       className={cn(
-        "fixed top-2 right-0 flex flex-col items-center justify-center w-6 md:w-20 h-20 md:h-40 group bg-slate-600 rounded-l-xl mt-5 p-2 z-50 transform transition-transform duration-300 ease-in-out hover:w-8 md:hover:w-24",
+        "fixed md:w-12 top-2 right-0 flex flex-col z-50 items-center justify-center w-5 h-32 group bg-slate-600 rounded-l-md dark:border-gray-100 mt-5 p-2 hover:scale-x-110",
         {
-          "w-8 h-10 mr-2 p-0 bg-transparent": isOpen,
+          "w-8 h-10 mr-2 p-0 mt-2 mb-0 bg-transparent": isOpen,
         }
       )}
       onClick={toggleMenu}
     >
-      <span className="sr-only">Toggle navigation menu</span>
-      <span className="hidden">navigation menu</span>
       <span
-        className={cn(genericHamburgerLine, {
-          "rotate-45 translate-y-3 bg-white max-w-8": isOpen,
-        })}
+        className={cn(
+          genericHamburgerLine,
+          { "md:w-0": !isOpen },
+          {
+            "rotate-45 translate-y-3 bg-white": isOpen,
+          }
+        )}
       />
       <span
-        className={cn(genericHamburgerLine, {
-          "opacity-0 group-hover:opacity-0 bg-white max-w-8": isOpen,
-        })}
+        className={cn(
+          genericHamburgerLine,
+          { "md:w-0": !isOpen },
+          {
+            "opacity-0 group-hover:opacity-0 bg-white": isOpen,
+          }
+        )}
       />
       <span
-        className={cn(genericHamburgerLine, {
-          "-rotate-45 -translate-y-3 bg-white max-w-8": isOpen,
-        })}
+        className={cn(
+          genericHamburgerLine,
+          { "md:w-0": !isOpen },
+          {
+            "-rotate-45 -translate-y-3 bg-white": isOpen,
+          }
+        )}
       />
-
-      <span className={cn("hidden text-gray-200", { "md:block": !isOpen })}>
+      <span
+        className={cn("hidden text-gray-200", { "md:block h-full": !isOpen })}
+      >
         ☰ القائمة
       </span>
     </button>
