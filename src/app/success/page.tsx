9ea5cfcf119001c5b5ce07061b1c1ad38c5d5071
adelpro <adelpro@gmail.com>
2024-08-25 21:49:00 +0100

    - adding redirect success after offline data download

---
+++ src/app/success/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Success from "./Success";
+
+export async function generateMetadata() {
+  const title = "تمت العملية بنجاح";
+  const description = "تمت العملية بنجاح";
+
+  return {
+    title,
+    description,
+  };
+}
+export default function Page() {
+  return <Success />;
+}
--- src/hooks/useDownloadassetsOffLine.ts
+++ src/hooks/useDownloadassetsOffLine.ts
@@ -1,23 +1,50 @@
+"use client";
 import { useState } from "react";
 import { defaultNumberOfPages } from "@/data/quran-metadata/mushaf-elmadina-warsh-azrak/spec";
+import { useRouter } from "next/navigation";
 const useDownloadassetsOffLine = () => {
+  const router = useRouter();
   const [status, setStatus] = useState<string>("");
   const [progress, setProgress] = useState<number>(0);
   const total = defaultNumberOfPages;
 
   const downloadAssets = async () => {
+    const CACHE_NAME = "mushaf-elmadina-warsh-azrak-cache";
+
+    const CACHE_EXPIRY = 6 * 30 * 24 * 60 * 60 * 1000;
+
     setStatus("بدء التحميل...");
     // fetch all images in public/mushaf/mushaf-elmadina-warsh-azrak from 1.png to 604.png the the baground
-    const cache = await caches.open("mushaf-elmadina-warsh-azrak-cache");
-    let progress = 0;
+
+    const cache = await caches.open(CACHE_NAME);
+    const cacheKeys = await cache.keys();
+    const cachedUrls = new Set(cacheKeys.map((request) => request.url));
+
+    let progressCount = 0;
 
     for (let i = 1; i <= total; i++) {
       const url = `/mushaf/mushaf-elmadina-warsh-azrak/${i}.png`;
+      // Check if the URL is already cached and not expired
+      if (cachedUrls.has(url)) {
+        const cacheResponse = await cache.match(url);
+        if (cacheResponse) {
+          const cacheDate = new Date(cacheResponse.headers.get("date") || "");
+          const now = Date.now();
+          if (now - cacheDate.getTime() < CACHE_EXPIRY) {
+            progressCount++;
+            setProgress(progressCount);
+            setStatus(`الصورة موجودة (${progressCount}/${total})...`);
+            continue;
+          } else {
+            await cache.delete(url); // Remove expired cache
+          }
+        }
+      }
       try {
         const response = await fetch(url);
         if (response.ok) {
           await cache.put(url, response);
-          progress++;
+          progressCount++;
           setProgress(progress);
           setStatus(`تحميل الصورة (${progress}/${total})...`);
         } else {
@@ -26,6 +53,11 @@ const useDownloadassetsOffLine = () => {
       } catch {}
     }
     setStatus("تم التحميل بنجاح");
+
+    // Redirect to the success page after 100ms
+    setTimeout(() => {
+      router.push("/success"); // Replace '/success' with the actual success page route
+    }, 100);
   };
   return {
     downloadAssets,
