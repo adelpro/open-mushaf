6d39b0e4d1460b651a4347b9aeabccddbf7981a3
adelpro <adelpro@gmail.com>
2024-08-31 15:24:00 +0100

    docs(commitlint)): commenting different type of commits

--- commitlint.config.js
+++ commitlint.config.js
@@ -14,23 +14,15 @@ module.exports = {
       'always',
 
       [
-        'feat',
-
-        'fix',
-
-        'docs',
-
-        'style',
-
-        'refactor',
-
-        'perf',
-
-        'test',
-
-        'chore',
-
-        'revert',
+        'feat', // New feature or functionality
+        'fix', // Bug fix or correction
+        'docs', // Documentation changes
+        'style', // Code style changes (e.g., formatting, whitespace)
+        'refactor', // Code refactoring without changing functionality
+        'perf', // Performance improvements
+        'test', // Adding or updating tests
+        'chore', // Routine tasks like build process or dependency updates
+        'revert', // Reverting a previous commit
       ],
     ],
   },
